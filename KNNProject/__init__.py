6b5512e980751216fe759c7e0edf83a374e33fa1
Jonathan Espinosa <jonespinosa89@gmail.com>
2023-04-17 21:25:05 -0400

    upload handler

--- KNNProject/__init__.py
+++ KNNProject/__init__.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 from KNNProject.knn import model, cReport
+import os
 
 app = Flask(__name__)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    image = request.files['image'];
+    image.save('./upload/test.jpg')
+    return 'success!'
 
 @app.route("/")
 def hello_world():
     return render_template('content.html', report=cReport);
+
